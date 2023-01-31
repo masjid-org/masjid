f1ca73d501c8aa67dfb05ac20512da935d7a4d47
Maruf Maniruzzaman <kuasha@gmail.com>
2023-01-31 08:53:16 -0500

    Added GPS coordinate fetching

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { Audio } from 'expo-av';
+import * as Location from 'expo-location';
+
 import PrayTimes from './PrayTimes';
 
 export default function App() {
   const [sound, setSound] = React.useState();
+  const [location, setLocation] = React.useState(null);
+  const [errorMsg, setErrorMsg] = React.useState(null);
+
   const prayTimes = new PrayTimes();
   const timeNames = ['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha', 'Midnight'];
 
@@ -18,7 +23,19 @@ export default function App() {
     await sound.playAsync();
   }
 
+  React.useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
 
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
   React.useEffect(() => {
     return sound
@@ -29,8 +46,16 @@ export default function App() {
       : undefined;
   }, [sound]);
 
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(Math.round(location.coords.latitude) + "," + Math.round(location.coords.longitude));
+  }
+
+
   var times = []
-  var pt = prayTimes.getTimes(new Date(), [23, 90], 0+6);
+  var pt = prayTimes.getTimes(new Date(), [location.coords.latitude, location.coords.longitude], 0+6);
 
   for(var i = 0; i < timeNames.length; i++){
     times.push(<Text>{timeNames[i]}: </Text>)
@@ -40,7 +65,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Player times in Bangladesh</Text>
+      <Text>{text}</Text>
       <Text>Date: {new Date().toLocaleDateString()}</Text>
       {times}
       <Button title="Play Azan" onPress={playSound} />
