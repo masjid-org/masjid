92a3ae299c8473f6c922872b0d3e29330da31dc2
Maruf Maniruzzaman <kuasha@gmail.com>
2023-01-31 08:09:10 -0500

    Added prayer time for fixed co-ordinate

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { Audio } from 'expo-av';
+import PrayTimes from './PrayTimes';
 
 export default function App() {
   const [sound, setSound] = React.useState();
+  const prayTimes = new PrayTimes();
+  const timeNames = ['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha', 'Midnight'];
 
-  async function playSound() {
+  async function playSound() {    
     console.log('Loading Sound');
     const { sound } = await Audio.Sound.createAsync( require('./assets/azan2.mp3')
     );
@@ -14,6 +17,8 @@ export default function App() {
     console.log('Playing Sound');
     await sound.playAsync();
   }
+
+
 
   React.useEffect(() => {
     return sound
@@ -24,8 +29,20 @@ export default function App() {
       : undefined;
   }, [sound]);
 
+  var times = []
+  var pt = prayTimes.getTimes(new Date(), [23, 90], 0+6);
+
+  for(var i = 0; i < timeNames.length; i++){
+    times.push(<Text>{timeNames[i]}: </Text>)
+    times.push(<Text>{pt[timeNames[i].toLowerCase()]}</Text>)
+  }
+
+
   return (
     <View style={styles.container}>
+      <Text>Player times in Bangladesh</Text>
+      <Text>Date: {new Date().toLocaleDateString()}</Text>
+      {times}
       <Button title="Play Azan" onPress={playSound} />
     </View>
   );
